71d79becec1c3c421246639760e3f2d2cdcd8ac5
doritozbae <doritosbae@mail.ru>
2021-08-23 01:18:39 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+   if (n < 0 ) {
+      n = -n;
+   }
+   let digits = n.toString().split('');
+   let result = digits.reverse(); 
+   return result.join('');
 }
